f4d8b06a4f95a31b00813a9613aa0155ef855229
Programmer001 <petko.hupka@gmail.com>
2023-12-28 11:45:03 +0100

    ...

--- SerialX-core/src/main/java/org/ugp/serialx/protocols/AutoProtocol.java
+++ SerialX-core/src/main/java/org/ugp/serialx/protocols/AutoProtocol.java
@@ -4,7 +4,6 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.ugp.serialx.Scope;
